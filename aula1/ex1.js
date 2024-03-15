59de1bc5e2332e32063cb2984cdc1a23531011c3
KauanGavinho <gavinhods@gmail.com>
2024-03-15 13:58:30 -0300

    updt1

---
+++ aula1/ex1.js
@@ -0,0 +1,13 @@
+let x=10, y=14
+
+console.log(x + y)
+console.log(x - y)
+console.log(x / y)
+console.log(x % y)
+console.log(x ** y)
+console.log(x += y)
+console.log(x -= y)
+console.log(x *= y)
+console.log(x /= y)
+console.log(x ++ )
+console.log(x -- )
