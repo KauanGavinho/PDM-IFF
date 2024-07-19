f5a4eefa325ffde3edb5c5164433d4b4fc41b70d
KauanGavinho <gavinhods@gmail.comgit config user.email gavinhods@gmail.comgit config user.email gavinhods@gmail.com>
2024-07-19 14:58:19 -0300

    updt

---
+++ aula2/exercicio/index.js
@@ -0,0 +1,50 @@
+/*(1)
+function bomdia(nome){
+    console.log(`Bom dia ${nome}!`)
+}
+bomdia('mano')
+*/
+
+/*(2)*/
+ const mes = ''
+ switch (mes) {
+    case '1':
+        console.log(Janeiro)
+        break;
+    case '2':
+        console.log(Fevereiro)
+        break;
+    case '3':
+        console.log(Fevereiro)
+        break;
+    case '4':
+        console.log(Fevereiro)
+        break;
+    case '5':
+        console.log(Fevereiro)
+        break;
+    case '6':
+        console.log(Fevereiro)
+        break; 
+    case '7':
+        console.log(Janeiro)
+        break;
+    case '8':
+        console.log(Fevereiro)
+        break;
+    case '9':
+        console.log(Fevereiro)
+        break;
+    case '10':
+        console.log(Fevereiro)
+        break;
+    case '11':
+        console.log(Fevereiro)
+        break;
+    case '12':
+        console.log(Fevereiro)
+        break;  
+ 
+    default:
+        break;
+ }
